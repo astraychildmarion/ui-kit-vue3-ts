6177304f41701ace852ac71e90e28bfbdcd25c6c
Marion Ma <marion.ma@yitmh.com>
2021-10-21 11:47:47 +0800

    searchbar finish

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+import xycloud from './xycloud';
+
+addons.setConfig({
+  theme: xycloud,
+  panelPosition: 'right',
+});
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <Input v-model:value="searchContent" id="xy-search-bar" @pressEnter="searchBarEnter">
+    <template #prefix>
+      <SearchOutlined />
+    </template>
+  </Input>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Input } from 'ant-design-vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
+
+interface TargetType {
+  target: { value: string };
+}
+
+export default defineComponent({
+  components: { Input, SearchOutlined },
+  emits: ['searchBarEnter'],
+  setup(props, { emit }) {
+    const searchContent = ref<string>('');
+    function searchBarEnter({ target }: TargetType) {
+      emit('searchBarEnter', target.value);
+    }
+    return {
+      searchContent,
+      searchBarEnter,
+    };
+  },
+});
+</script>
