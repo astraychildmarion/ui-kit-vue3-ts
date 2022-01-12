bc7ba74e3ec0c286a181bf35f1f5428d300eb6b4
Marion Ma <marion.ma@yitmh.com>
2022-01-12 15:43:34 +0800

    Revert "feat(action):add customize text when no checkbox checked"
    This reverts commit 7ad7503d28d90b678f6fa19b2a6b4bd89e83d752.

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -22,7 +22,6 @@
           :isToolbar="true"
           :actionOption="actionOption"
           :isTableCheckbox="isTableCheckbox"
-          :noCheckboxText="noCheckboxText"
           @clickAction="clickAction"
         />
       </Space>
@@ -113,10 +112,6 @@ export default defineComponent({
     },
     exportExcelOption: {
       type: Array as PropType<ExportExcelDropdownData[]>,
-    },
-    noCheckboxText: {
-      default: 'One or more items from server list must be selected.',
-      type: String as PropType<string>,
     },
     isTableCheckbox: {
       type: Boolean,
